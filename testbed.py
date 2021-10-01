deb8308c92d7b4af9e6c1627af66beb03cebe13c
Shashank D <shashankindiamanoj@gmail.com>
2021-10-01 23:45:38 +0530

    Update testbed script

--- testbed.py
+++ testbed.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2021 NITK Surathkal
+
 import time
 from multiprocessing import Process
 from subprocess import PIPE
@@ -10,27 +13,29 @@ config.set_value("assign_random_names", False)
 ##############################
 # Topology
 #
-# dhcp_client --><- Switch -><-- bng_router --><-- dhcp_server
+# dhcp_client -><-- bng_router --><-- dhcp_server
 ##############################
 
 dhcp_client = Node("cli")
-switch = Switch("swi")
 bng_router = Node("bng")
 dhcp_server = Node("ser")
-
+print(exec_subprocess(f"make", output=True))
 
 # Enabling IP forwarding for the routers
 bng_router.enable_ip_forwarding()
 
 
-(client_if, sw_cl_if) = connect(dhcp_client, switch, "cl", "s_c")
-(bng_sw_if, sw_bng_if) = connect(bng_router, switch, "b_s", "s_b")
+(client_if, bng_cl_if) = connect(dhcp_client, bng_router, "cl", "b_c")
 (server_if, router_server_if) = connect(dhcp_server, bng_router, "s_r", "r_s")
-bng_sw_if.set_address("10.0.0.1/24")
+bng_cl_if.set_address("10.0.0.1/24")
 
 with dhcp_client:
-    print(exec_subprocess(f"ip link add link {client_if.id} name clv type vlan id 20"))
-    print(exec_subprocess(f"ip link add link clv name clv.30 type vlan id 30"))
+    print(exec_subprocess(f"ip link add link {client_if.id} name clv type vlan id 30"))
+    print(exec_subprocess(f"ip link set clv up"))
+    print(exec_subprocess(f"ip link add link clv name clv.20 type vlan id 20 "))
+    print(exec_subprocess(f"ip link set clv.20 up"))
+
+
 
 client_if.set_address("10.0.0.2/24")
 router_server_if.set_address("10.0.1.1/24")
@@ -39,7 +44,7 @@ server_if.set_address("10.0.1.2/24")
 
 # Start dhcclient in client
 with dhcp_client:
-    cmd = "dhclient -d --decline-wait-time 1"
+    cmd = "dhclient -d --decline-wait-time 1 clv.20"
     dhcp_worker = Process(target=exec_subprocess, args=(cmd,))
     dhcp_worker.start()
 
@@ -71,7 +76,7 @@ dhcp_server.add_route("DEFAULT", server_if)
 # Attach xdp program
 cmd = (
     f"ip netns exec {bng_router.id} ./dhcp_user_xdp "
-    f"-i {bng_sw_if.id} -d {server_if.address.get_addr(with_subnet=False)}"
+    f"-i {bng_cl_if.id} -d {server_if.address.get_addr(with_subnet=False)}"
 )
 print(exec_subprocess(cmd, output=True))
 
@@ -80,6 +85,21 @@ print(exec_subprocess(cmd, output=True))
 #     dhcp_worker = Process(target=exec_subprocess, args=(cmd,))
 #     dhcp_worker.start()
 
+xdp_dump_workers = [
+    Process(
+        target=exec_exp_commands,
+        args=(
+            f"ip netns exec {node.id} ./xdp-tools/xdp-dump/xdpdump -i b_c -w b_c.pcap",
+            PIPE,
+            PIPE,
+            400,
+        ),
+    )
+    for node in [bng_router]
+]
+
+for wrker in xdp_dump_workers:
+    wrker.start()
 
 shark_workers = [
     Process(
@@ -92,6 +112,23 @@ shark_workers = [
         ),
     )
     for node in [bng_router]
+]
+
+for wrker in shark_workers:
+    wrker.start()
+
+
+shark_workers = [
+    Process(
+        target=exec_exp_commands,
+        args=(
+            f"ip netns exec {node.id} wireshark",
+            PIPE,
+            PIPE,
+            400,
+        ),
+    )
+    for node in [dhcp_client]
 ]
 
 for wrker in shark_workers:
